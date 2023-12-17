16b40ae41061723ab2b051d96c6034936cfa9f37
陈昊天 <2363822647@qq.com>
2023-12-17 15:31:19 +0800

    Yawn detection

--- src/views/User/Controller/VideoSurveillance.vue
+++ src/views/User/Controller/VideoSurveillance.vue
@@ -1,21 +1,25 @@
 <template>
     <div>
-    <div>
-      <!--面包屑-->
-      <el-breadcrumb separator-class="el-icon-arrow-right" style="padding-left: 10px;padding-bottom: 10px;font-size: 12px">
-        <el-breadcrumb-item :to="{ path: '/usermain' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>监控中心</el-breadcrumb-item>
-        <el-breadcrumb-item>视频监控</el-breadcrumb-item>
-      </el-breadcrumb>
-    </div>
+        <div>
+            <!--面包屑-->
+            <el-breadcrumb separator-class="el-icon-arrow-right"
+                style="padding-left: 10px;padding-bottom: 10px;font-size: 12px">
+                <el-breadcrumb-item :to="{ path: '/usermain' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item>监控中心</el-breadcrumb-item>
+                <el-breadcrumb-item>视频监控</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
         <video ref="videoElement" autoplay style="display: none;"></video>
         <img :src="imageSrc" />
         <div>
-            <p>Fatigue Count: {{ fatigueCount }}</p>
-            <p>Status: {{ status }}</p>
+            <p>眯眼计数: {{ eye_count }}</p>
+            <p>眼部状态: {{ eye_text }}</p>
+            <p>打哈欠计数: {{ mouth_count }}</p>
+            <p>嘴部状态: {{ mouth_text }}</p>
         </div>
     </div>
 </template>
+
   
 <script>
 import io from 'socket.io-client';
@@ -44,6 +48,9 @@ export default {
                 const canvas = document.createElement('canvas');
                 const context = canvas.getContext('2d');
 
+                canvas.width = 600; // 设置宽度为1280像素
+                canvas.height = 400; // 设置高度为720像素
+
                 setInterval(() => {
                     context.drawImage(this.videoElement, 0, 0, canvas.width, canvas.height);
                     const dataURL = canvas.toDataURL('image/jpeg');
@@ -54,8 +61,10 @@ export default {
 
         this.socket.on('response', (data) => {
             this.imageSrc = 'data:image/jpeg;base64,' + data.data; // Update the image source
-            this.fatigueCount = data.count;
-            this.status = data.status;
+            this.eye_count = data.eye_count;
+            this.eye_text = data.eye_text;
+            this.mouth_count = data.mouth_count;
+            this.mouth_text = data.mouth_text;
         });
     },
     beforeDestroy() {
@@ -67,7 +76,8 @@ export default {
 <style>
 img {
     width: 80%;
-    max-width: 720px;
+    max-width: 600px;
     height: auto;
 }
 </style>
+
