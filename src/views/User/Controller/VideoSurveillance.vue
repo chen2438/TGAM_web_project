583ec5de953c46f4a51857c2cb43f3a60e90d6e1
陈昊天 <2363822647@qq.com>
2023-12-18 22:19:29 +0800

    Fatigue driving reminder

--- src/views/User/Controller/VideoSurveillance.vue
+++ src/views/User/Controller/VideoSurveillance.vue
@@ -1,47 +1,31 @@
 <template>
     <div>
-    <div>
-      <!--面包屑-->
-      <el-breadcrumb separator-class="el-icon-arrow-right" style="padding-left: 10px;padding-bottom: 10px;font-size: 12px">
-        <el-breadcrumb-item :to="{ path: '/usermain' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>监控中心</el-breadcrumb-item>
-        <el-breadcrumb-item>视频监控</el-breadcrumb-item>
-      </el-breadcrumb>
-    </div>
-    <div style="margin-bottom: 30px;">
-        <img src="../../User/camera.png" style="width: 80px;margin-left: 370px;">
-        <span style="font-size: 80px;color:rgb(106, 165, 190);"> 摄   像  检  测</span>
-    </div>
-
-    <el-container>
-  <el-aside width="600px" style="margin-left: 40px;">
-
-    <video ref="videoElement" autoplay style="display: none;"></video>
-    <img :src="imageSrc" />
-
-  </el-aside>
-  <el-main>
-    <el-table :data="tableData" class="img-styled" style="margin-top: 30px;">
+        <div>
+            <!--面包屑-->
+            <el-breadcrumb separator-class="el-icon-arrow-right"
+                style="padding-left: 10px;padding-bottom: 10px;font-size: 12px">
+                <el-breadcrumb-item :to="{ path: '/usermain' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item>监控中心</el-breadcrumb-item>
+                <el-breadcrumb-item>视频监控</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <el-alert v-if="showAlert" title="疲劳警告" type="warning" description="检测到频繁眯眼或打哈欠，可能存在疲劳驾驶风险。" show-icon>
+        </el-alert>
+        <video ref="videoElement" autoplay style="display: none;"></video>
+        <img :src="imageSrc" class="img-styled" />
+        <el-table :data="tableData" class="img-styled">
             <el-table-column prop="label" label="项目"></el-table-column>
             <el-table-column prop="value" label="数据"></el-table-column>
-    </el-table>
-  </el-main>
-</el-container>
-<div>
-        <audio controls="controls"  src="../alarm_music/1级.mp3" ref="audio"></audio>
-        <audio controls="controls" src="../alarm_music/2级.mp3"  ref="audio"></audio>
-        <audio controls="controls" src="../alarm_music/3级.mp3"  ref="audio"></audio>
-        <audio controls="controls" src="../alarm_music/大哥清醒一下.mp3"  ref="audio"></audio> <!--测试mountd函数加载后音乐器是否播放-->
-        <audio controls="controls"></audio>
+        </el-table>
 
-</div>
-
-</div>
+    </div>
 </template>
+
+
   
 <script>
 import io from 'socket.io-client';
-import { updateVideo } from '../../../api/data';
+
 export default {
     name: 'UserFatigueDetection',
     data() {
@@ -49,14 +33,8 @@ export default {
             videoElement: null,
             outputElement: null,
             imageSrc: '',
-            eye_count: 0,
-            temp_eye:0,
-            eye_text: '',
-            mouth_count: 0,
-            temp_mouth:0,
-            mouth_text: '',
             fatigueCount: 0,
-            status: {},
+            status: 'Normal',
             socket: io('http://localhost:8000'), // Replace with your server URL and port
             tableData: [
                 { label: '眯眼计数', value: '' },
@@ -64,9 +42,9 @@ export default {
                 { label: '打哈欠计数', value: '' },
                 { label: '嘴部状态', value: '' }
             ],
-            reminded:0,
-            count:0,
-            audioSrc:''
+            lastEyeCount: 0,
+            lastMouthCount: 0,
+            showAlert: false, // 控制提醒卡片的显示
         };
 
     },
@@ -79,46 +57,15 @@ export default {
                 { label: '嘴部状态', value: this.mouth_text }
             ];
         },
-        async updateVideo(){
-            console.log("updateVideo前的status" + this.status)
-            const response = await updateVideo(this.status)
-            if (response.code === 20000) console.log("更新数据成功")
-        },
-        checkFatigue() {
-            const eyeCount = this.eye_count;
-            const yawnCount = this.mouth_count;
-            if (eyeCount <= 5 && yawnCount <= 1) {
-                // 不疲劳
-                this.audioSrc = '';
-            } else if (
-                (eyeCount > 5 && eyeCount <= 10) || (yawnCount > 2 && yawnCount <= 4)) {
-                // 轻度疲劳
-                this.audioSrc = '../alarm_music/大哥清醒一下.mp3';
-            } else if ((eyeCount > 10 && eyeCount <= 15) || (yawnCount > 4 && yawnCount <= 6)) {
-                // 重度疲劳
-                this.audioSrc = '../alarm_music/1级.mp3';
-                this.fatigueCount++;
-            } else if (eyeCount > 15 || yawnCount > 6) {
-                // 极度疲劳
-                this.audioSrc = '../alarm_music/大哥清醒一下.mp3,../alarm_music/3级.mp3';
-                this.fatigueCount++;
-            }
-            },
-        playAudio(audioSrc) {
-            const audio = new Audio(audioSrc);
+        playAlertSound() {
+            const audio = new Audio('/audio/remind.mp3'); // 音频文件路径
             audio.play();
-            this.reminded++;
-            },
-        startplay() {
-            this.$refs.audio.currentTime = 0; // 从头开始播放提示音
-            this.$refs.audio.play(); // 播放
-            console.log('播放');
-            },
+        }
     },
     mounted() {
         this.videoElement = this.$refs.videoElement;
         this.outputElement = this.$refs.outputElement;
-        this.startplay()//测试音乐器能否播放
+
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
                 this.videoElement.srcObject = stream;
@@ -127,8 +74,8 @@ export default {
                 const canvas = document.createElement('canvas');
                 const context = canvas.getContext('2d');
 
-                canvas.width = 600; // 设置宽度为1280像素
-                canvas.height = 400; // 设置高度为720像素
+                canvas.width = 600;
+                canvas.height = 400;
 
                 setInterval(() => {
                     context.drawImage(this.videoElement, 0, 0, canvas.width, canvas.height);
@@ -137,35 +84,6 @@ export default {
                 }, 100); // Send frames every 100ms
             });
         }
-        console.log("***********************************************")
-                    // 每30秒执行一次判断
-        setInterval(() => {
-            this.count++;
-            this.checkFatigue();
-            if (this.audioSrc) { this.playAudio(this.audioSrc.trim());}
-            this.audioSrc = ''
-            this.temp_eye += this.eye_count;
-            this.temp_mouth + this.mouth_count;
-            if (this.count === 4) {
-                this.status = {
-                    time: new Date().toLocaleTimeString().strftime("%Y-%m-%d %H:%M"),
-                    fatigue: this.fatigueCount,
-                    situation: `眯眼${this.temp_eye}次, 打哈欠${this.temp_mouth}次`,
-                    reminded:this.reminded,
-                    userId:this.$cookies.get("userId"),
-                    userName:this.$cookies.get("userName"),
-                    carPlates:this.$cookies.get("carPlates")
-                },
-                this.temp_eye = 0;
-                this.temp_mouth = 0;
-                this.reminded = 0;
-                this.fatigueCount = 0;
-                this.count = 0;
-                this.updateVideo();
-            }
-        },30000);
-        console.log("***********************************************")
-
 
         this.socket.on('response', (data) => {
             this.imageSrc = 'data:image/jpeg;base64,' + data.data; // Update the image source
@@ -174,13 +92,19 @@ export default {
             this.mouth_count = data.mouth_count;
             this.mouth_text = data.mouth_text;
             this.updateTableData(); // 更新表格数据
+            // 检查眯眼或打哈欠计数是否递增1
+            if (data.eye_count - this.lastEyeCount >= 1 || data.mouth_count - this.lastMouthCount >= 1) {
+                this.showAlert = true; // 显示警告卡片
+                this.playAlertSound(); // 播放警告声音
+            }
+            // 更新上一次的计数
+            this.lastEyeCount = data.eye_count;
+            this.lastMouthCount = data.mouth_count;
         });
     },
     beforeDestroy() {
         this.socket.disconnect();
-        clearInterval(this.timer)
     }
-
 };
 </script>
   
